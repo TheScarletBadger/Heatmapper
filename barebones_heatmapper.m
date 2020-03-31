57379dfe4fb0e34c257f07dc63a23526edb2aae7
TheScarletBadger <56405836+TheScarletBadger@users.noreply.github.com>
2020-03-31 15:14:25 +0100

    Add files via upload

---
+++ barebones_heatmapper.m
@@ -0,0 +1,28 @@
+clear all
+%Edit this part------------------------------------------------------------
+source = 'C:\Users\Barry\Desktop\input';
+nbin = 50;
+%--------------------------------------------------------------------------
+
+%list contents of source directory
+D = dir(source);
+%remove entries for working and parent directories
+D = D(~ismember({D.name},{'.','..'}));
+%initialize empty array
+carray = [];
+%iterate over files in source directory, append x-y pairs to carray
+for i = 1:length(D)
+    carray = vertcat(carray,readmatrix([source filesep D(i).name]));
+end
+%remove extraneous data and NAN
+carray = carray(sum(isnan(carray),2)==0,2:3);
+%remove 0,0 pairs (tracking lost)
+carray = carray(sum(carray,2)~=0,:);
+%convert to nbins by nbins array of n x-y pairs inside each bin
+zarray = hist3(carray,'nbins',[nbin nbin]);
+%display as surface with default colormap and intepolated edge / face color
+surf(zarray,'FaceColor','interp', 'EdgeColor', 'interp')
+colormap('jet');
+axis equal
+axis off
+view(2)
